1031cce38abdf704aec63a637ccadccd1429c670
charlie sando <charlie.fats@gmail.com>
2022-03-20 17:42:00 -0400

    fix: fixing imports

--- pyprql/__init__.py
+++ pyprql/__init__.py
@@ -2,6 +2,6 @@
 """Source code for PyPrql."""
 __version__ = "0.1.2"
 
-import lang.prql as prql  # noqa: F401
+import pyprql.lang.prql as prql  # noqa: F401
 
-from .lang.prql import to_sql as to_sql  # noqa: F401
+from pyprql.lang.prql import to_sql as to_sql  # noqa: F401
