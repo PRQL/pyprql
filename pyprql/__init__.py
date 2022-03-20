e8080d4bf8b39a2a043820a8d8f58f2070b03fd0
charlie sando <charlie.fats@gmail.com>
2022-03-20 17:37:34 -0400

    fix: clean-up for redistribution

--- pyprql/__init__.py
+++ pyprql/__init__.py
@@ -2,6 +2,6 @@
 """Source code for PyPrql."""
 __version__ = "0.1.2"
 
-import lang.prql as prql
+import lang.prql as prql  # noqa: F401
 
-from .lang.prql import to_sql as to_sql
+from .lang.prql import to_sql as to_sql  # noqa: F401
