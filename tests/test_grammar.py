3c68f01f322ab5e9f8f3969c201a212d0651b75a
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-04-08 11:18:21 +0100

    test(test_grammar.py): correct index
    The searched for string in the test_aggregates_grammar_nested should occur at position == 0, not > 0.

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -31,88 +31,85 @@ class TestSqlGenerator(unittest.TestCase):
     def test_expressions_in_filter(self):
         # Songs greater than 5 minutes
 
-        q = '''
+        q = """
         from t:tracks
         select t.Composer
         filter t.Milliseconds * 1000 > 5 * 60
         take 23
-        '''
+        """
         res = prql.to_sql(q)
-        assert res.index('t.Milliseconds*1000>5*60') > 0
+        assert res.index("t.Milliseconds*1000>5*60") > 0
         self.run_query(q, 23)
 
     def test_expressions_in_filter_w_parens(self):
         # Songs greater than 5 minutes
 
-        q = '''
+        q = """
         from t:tracks
         select t.Composer
         filter (t.Milliseconds * 1000) > 5 * 60
         take 23
-        '''
+        """
         res = prql.to_sql(q)
         print(res)
-        assert res.index('(t.Milliseconds*1000)>5*60') > 0
+        assert res.index("(t.Milliseconds*1000)>5*60") > 0
         self.run_query(q, 23)
 
     def test_expression_in_derive(self):
-        q = '''
-        from t:tracks 
-        join a:albums [ AlbumId ] 
-        join mt:media_types [ MediaTypeId ]  
-        join g:genres [ GenreId ] 
-        derive [ 
-            track: t.Name, 
-            minutes: (t.Milliseconds / 1000.0) / 60.0 
+        q = """
+        from t:tracks
+        join a:albums [ AlbumId ]
+        join mt:media_types [ MediaTypeId ]
+        join g:genres [ GenreId ]
+        derive [
+            track: t.Name,
+            minutes: (t.Milliseconds / 1000.0) / 60.0
         ]
         select t.Composer
-        '''
+        """
         sql = self.run_query(q)
-        assert sql.index('(t.Milliseconds/1000.0)/60.0') > 0
+        assert sql.index("(t.Milliseconds/1000.0)/60.0") > 0
 
     def test_expression_in_derive_without_parens(self):
-        q = '''
-        from t:tracks 
-        join a:albums [ AlbumId ] 
-        join mt:media_types [ MediaTypeId ]  
-        join g:genres [ GenreId ] 
-        derive [ 
-            track: t.Name, 
-            minutes: t.Milliseconds / 1000.0 / 60.0 
+        q = """
+        from t:tracks
+        join a:albums [ AlbumId ]
+        join mt:media_types [ MediaTypeId ]
+        join g:genres [ GenreId ]
+        derive [
+            track: t.Name,
+            minutes: t.Milliseconds / 1000.0 / 60.0
         ]
         select t.Composer
-        '''
+        """
         sql = self.run_query(q)
-        assert sql.index('t.Milliseconds/1000.0/60.0') > 0
+        assert sql.index("t.Milliseconds/1000.0/60.0") > 0
 
     def test_hypothetical_with_lots_of_sub_expressions(self):
-        q = '''
+        q = """
         from tracks
-        filter t.Milliseconds * (t.TrackId * (32/(47/72)+(1/8))) > 5 * 60.01 + 3.1 
-        '''
+        filter t.Milliseconds * (t.TrackId * (32/(47/72)+(1/8))) > 5 * 60.01 + 3.1
+        """
         sql = prql.to_sql(q)
         print(sql)
-        assert sql.index('t.Milliseconds*(t.TrackId*(32/(47/72)+(1/8)))') > 0
-
-
+        assert sql.index("t.Milliseconds*(t.TrackId*(32/(47/72)+(1/8)))") > 0
 
     def test_aggregates_grammar(self):
-        q = '''
+        q = """
         from tracks
-        aggregate  by:t.Composer [ 
+        aggregate  by:t.Composer [
             row_count: t.TrackId | count
         ]
-        '''
+        """
         sql = prql.to_sql(q)
         print(sql)
 
     def test_aggregates_grammar_nested(self):
-        q = '''
+        q = """
         from tracks
-        aggregate  by:t.Composer [ 
-            row_count: (t.TrackId | count) | max 
+        aggregate  by:t.Composer [
+            row_count: (t.TrackId | count) | max
         ]
-        '''
+        """
         sql = prql.to_sql(q)
-        assert sql.index('SELECT MAX(COUNT(t.TrackId))') > 0
-        #print(sql)
+        assert sql.index("SELECT MAX(COUNT(t.TrackId))") == 0
