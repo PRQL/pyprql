ed0be45fae4c65b8cec34a83bc620f39f3c8a362
charlie sanders <711385+charlie-sanders@users.noreply.github.com>
2022-08-11 15:56:23 -0500

    Add pandas dataframe accessor for using PRQL with Pandas (#67)
    * Adding PRQL support for Pandas via accessor

---
+++ pyprql/pandas/prql.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Pandas Dataframe accessor for PRQL.
+
+Examples
+--------
+import  pandas as pd
+import pyprql.pandas
+df = pd.DataFrame({})
+results_df = df.prql.query('from df | select [age,name,occupation] | filter age > 21')
+
+"""
+
+import duckdb
+import pandas as pd
+import prql_python as prql
+
+
+@pd.api.extensions.register_dataframe_accessor("prql")
+class PrqlAccessor:
+    def __init__(self, pandas_obj: object) -> None:
+        self._obj = pandas_obj
+
+    def query(self, prql_query: str) -> pd.DataFrame:
+        return duckdb.query(prql.to_sql(prql_query)).to_df()
