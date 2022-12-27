6f6b57a322fad926cf8f625e3573912a3a91b13f
Maximilian Roos <m@maxroos.com>
2022-12-26 20:33:12 -0800

    fix: Add py3.8-compatible typing
    This demonstrates we need tests for the magic...

--- pyprql/magic/prql.py
+++ pyprql/magic/prql.py
@@ -1,4 +1,6 @@
 """A magic class for parsing PRQL in IPython or Jupyter."""
+from __future__ import annotations
+
 from typing import Dict
 
 from IPython.core.magic import cell_magic, line_magic, magics_class, needs_local_scope
