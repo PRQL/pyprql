c313ff125152fc6a89bb8fa1b266a6cceb1764a6
eitsupi <ts1s1andn@gmail.com>
2024-04-29 04:14:00 +0000

    refactor: more useful type hint

--- pyprql/polars_namespace/prql.py
+++ pyprql/polars_namespace/prql.py
@@ -2,15 +2,19 @@ import polars as pl
 import prqlc
 
 
+from typing import TypeVar, Generic
+
+
+T_DF = TypeVar("T_DF", pl.DataFrame, pl.LazyFrame)
+
+
 @pl.api.register_dataframe_namespace("prql")
 @pl.api.register_lazyframe_namespace("prql")
-class PrqlNamespace:
-    def __init__(self, df: pl.DataFrame | pl.LazyFrame):
+class PrqlNamespace(Generic[T_DF]):
+    def __init__(self, df: T_DF):
         self._df = df
 
-    def query(
-        self, prql_query: str, *, table_name: str | None = None
-    ) -> pl.DataFrame | pl.LazyFrame:
+    def query(self, prql_query: str, *, table_name: str | None = None) -> T_DF:
         prepended_query: str = f"from self \n {prql_query}"
         sql_query: str = prqlc.compile(
             prepended_query,
